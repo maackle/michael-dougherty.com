a50feefc046d8761673f86bd3f47de0038df59e0
Michael Dougherty <michael@lv11.co>
2013-07-18 19:32:20 -0700

    static site

--- project/website/urls.py
+++ project/website/urls.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-from website.views import HomeView
+from website.views import *
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^web$', WebsitePageView.as_view(), name='web'),
+    url(r'^music$', MusicPageView.as_view(), name='music'),
+    url(r'^projects$', ProjectPageView.as_view(), name='projects'),
     # url(r'^website/', include('website.foo.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
