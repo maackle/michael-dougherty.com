ce5c558f9df893d13147c1b10b356b46134470e5
Michael Dougherty <michael@lv11.co>
2013-07-20 18:07:34 -0700

    first version

--- project/website/urls.py
+++ project/website/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^music$', MusicPageView.as_view(), name='music'),
     url(r'^projects$', ProjectPageView.as_view(), name='projects'),
     # url(r'^website/', include('website.foo.urls')),
-
+    # (r'^contact/',    include('envelope.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
